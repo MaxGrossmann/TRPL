1f23c8faf986cea7d1b73bb42df66209d3758ed0
Max Grossmann <max.grossmann@tu-ilmenau.de>
2022-01-13 10:59:33 +0100

    code comments

--- Code/eta0_calc.jl
+++ Code/eta0_calc.jl
@@ -2,23 +2,27 @@
 cd(@__DIR__)
 using Pkg
 Pkg.activate("Project.toml")
+
 ## physical constants
-h = 6.62607015*1e-34
+h = 6.62607015e-34
 c = 299792458
+
 ## experimental parameters
 rep_rate = 200e3
-P = [6.89e-8, 6.21e-4]
-lambda = 520*1e-9
-fwhm = 205*1e-6
-d = 2000*1e-9
-T = 0.4292
-N_DA = 5e16
+P        = [6.89e-8, 6.21e-4] # min P and max P of our laser
+lambda   = 520*1e-9
+fwhm     = 205*1e-6
+d        = 2000*1e-9
+T        = 0.4292 # calculated in transm_calc.jl
+N_DA     = 5e16
+
 ## main calculations
-E = P./rep_rate
-N = T*E/(h*c)*lambda
-V = pi*fwhm^2*d/4
-n = N/V * 1e-6
+E        = P./rep_rate
+N        = T*E/(h*c)*lambda
+V        = pi*fwhm^2*d/4
+n        = N/V * 1e-6
 fwhm_int = 0.76 # from fwhm_int.jl script
-eta_0 = round.(fwhm_int*n/N_DA,sigdigits=3)
+eta_0    = round.(fwhm_int*n/N_DA,sigdigits=3)
+
 ## print result
 println("\nη₀ ∈ [$(eta_0[1]),$(eta_0[end])]")
